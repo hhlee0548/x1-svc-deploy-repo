88868b887083647bba1d17b2f169bce9f2aa9d03
Ubuntu <cgshome2@gmail.com>
2018-06-14 13:26:22 +0900

    lb.sh is updated, removing lb.yml and vars/lb #1

--- bin/lb.sh
+++ bin/lb.sh
@@ -1,6 +1,8 @@
-ansible-playbook ../lb.yml \
+ansible-playbook ../keepalived.yml ../haproxy.yml \
   -e 'ansible_python_interpreter=/usr/bin/python3' \
-  -i ../vars/lb/hosts \
+  -i ../vars/keepalived/hosts \
+  -i ../vars/haproxy/hosts \
   --key-file=/tmp/lb.pem \
-  --extra-vars @../vars/lb/common.yml \
+  --extra-vars @../vars/keepalived/common.yml \
+  --extra-vars @../vars/haproxy/common.yml \
   -t install
