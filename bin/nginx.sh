b72f5f5f7f2b03dfb656220c8533e69ad2c703ea
Ubuntu <cgshome2@gmail.com>
2018-06-19 15:15:14 +0900

    nginx roles added; tags for start/stop/restart added for all roles

---
+++ bin/nginx.sh
@@ -0,0 +1,7 @@
+ansible-playbook ../nginx.yml \
+  -e 'ansible_python_interpreter=/usr/bin/python3' \
+  -i ../vars/nginx/hosts \
+  --key-file=/tmp/lb.pem \
+  --extra-vars @../vars/nginx/common.yml \
+  -t install \
+  -vv
