58627fbf4666f9ab1000797478ce9714ffc4c0e5
GunSik Choi <cgshome2@gmail.com>
2018-06-25 13:18:08 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 sudo pip3 install --upgrade pip
 sudo pip3 install pyopenssl
