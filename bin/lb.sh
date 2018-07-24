a5d5f1d1b03b29a128cc1508d7f3aa99803a74d8
Ubuntu <cgshome2@gmail.com>
2018-07-24 20:00:36 +0900

    feature: common tags (status, info, deploy) are added

--- bin/lb.sh
+++ bin/lb.sh
@@ -1,5 +1,5 @@
-if [[ "$1" == "" || "$1" != "install" && "$1" != "config" && "$1" != "remove" ]]; then
-    echo "$0 [install | config | remove]"
+if [[ "$1" == "" || "$1" != "install" && "$1" != "config" && "$1" != "remove" && "$1" != "stop" && "$1" != "start" && "$1" != "status" && "$1" != "restart" && "$1" != "info" && "$1" != "deploy" && "$1" != "debug" ]]; then
+    echo "$0 [install | config | remove | start | stop | restart | status | info | deploy]"
     exit 1
 fi
 
--- bin/tomcat.sh
+++ bin/tomcat.sh
@@ -1,7 +1,8 @@
-#if [[ "$1" == "" || "$1" != "install" && "$1" != "config" && "$1" != "remove" ]]; then
-#    echo "$0 [install | config | remove]"
-#    exit 1
-#fi
+if [[ "$1" == "" || "$1" != "install" && "$1" != "config" && "$1" != "remove" && "$1" != "stop" && "$1" != "start" && "$1" != "status" && "$1" != "restart" && "$1" != "info" && "$1" != "deploy" && "$1" != "debug" ]]; then
+    echo "$0 [install | config | remove | start | stop | restart | status | info | deploy]"
+    exit 1
+fi
+
 
 ansible-playbook tomcat.yml \
   -e 'ansible_python_interpreter=/usr/bin/python3' \
